3099a0560f8d58df9c1e0cdc02b7a0119e8f69c4
ST10267411 <125186956+AbdulDavids@users.noreply.github.com>
2023-06-11 17:04:58 +0200

    Update EQRScript.js

--- EQRScript.js
+++ EQRScript.js
@@ -1,38 +1,62 @@
 function generateQRCode() {
-    // Retrieve user input values
-    var name = document.getElementById("name").value;
-    var age = document.getElementById("age").value;
-    var bloodType = document.getElementById("bloodType").value;
-    var allergies = document.getElementById("allergies").value;
-    var medConditions = document.getElementById("medConditions").value;
-    var emergencyContact1 = document.getElementById("emergencyContact1").value;
+  // Retrieve user input values
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var bloodType = document.getElementById("bloodType").value;
+  var allergies = document.getElementById("allergies").value;
+  var medConditions = document.getElementById("medConditions").value;
+  var emergencyContact1 = document.getElementById("emergencyContact1").value;
 
-  
-    // Create user object with the input values
-    var userInfo = {
-      name: name,
-      age: age,
-      bloodType: bloodType,
-      allergies: allergies,
-      medConditions: medConditions,
-      emergencyContact1: emergencyContact1
-    };
-  
-    // Convert user object to JSON string
-    var userInfoJSON = JSON.stringify(userInfo);
-  
-    // Generate the QR code using a QR code library
-    var qrCode = new QRCode(document.getElementById("qrcode"), {
-      text: userInfoJSON,
-      width: 128,
-      height: 128
-    });
-  
-    // Create a download link for the QR code
-    var downloadLink = document.createElement("a");
-    downloadLink.href = document.getElementById("qrcode").getElementsByTagName("canvas")[0].toDataURL("image/jpeg");
-    downloadLink.download = "qrcode.jpg";
-    downloadLink.innerHTML = "Download QR Code";
-    document.getElementById("qrcode-container").appendChild(downloadLink);
+  if (!name){
+    name = "Unkown"
   }
-  
+
+if (!age){
+  age = "age unknown"
+}
+
+  if (!allergies) {
+    allergies = "no allergies";
+  } else {
+    allergies = " has " + allergies.toLowerCase() + " allergy";
+  }
+
+
+  if (!bloodType) {
+    bloodType = "unknown bloodtype";
+  } else {
+    bloodType = "Bloodtype: " + bloodType.toUpperCase();
+  }
+
+
+  if (!medConditions) {
+    medConditions = "no underlying medical conditions";
+  }
+
+
+  if(!emergencyContact1) {
+    emergencyContact1 = "N/A"
+  }
+  emergencyContact1 = "contact: " + emergencyContact1;
+
+
+
+
+  // Create user String with the input values
+  var userInfo = name + ", " + age + ", " + bloodType + ", " + allergies + ", " + medConditions + ", " + emergencyContact1;
+
+
+  // Generate the QR code using a QR code library
+  var qrCode = new QRCode(document.getElementById("qrcode"), {
+    text: userInfo,
+    width: 512,
+    height: 512,
+  });
+
+  // Create a download link for the QR code
+  var downloadLink = document.createElement("a");
+  downloadLink.href = document.getElementById("qrcode").getElementsByTagName("canvas")[0].toDataURL("image/jpeg");
+  downloadLink.download = "qrcode.jpg";
+  downloadLink.innerHTML = "Download QR Code";
+  document.getElementById("qrcode-container").appendChild(downloadLink);
+}
