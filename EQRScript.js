eff73681c7a3c0706c8e6f2809ec449dfb3bd445
ST10267411 <125186956+AbdulDavids@users.noreply.github.com>
2023-06-12 10:28:05 +0200

    Update EQRScript.js

--- EQRScript.js
+++ EQRScript.js
@@ -1,4 +1,12 @@
 function generateQRCode() {
+
+  const box = document.getElementById("qrcode");
+
+  if (box.childNodes.length > 0){
+    document.getElementById(box).innerHTML = "";
+  }
+  
+
   // Retrieve user input values
   var name = document.getElementById("name").value;
   var age = document.getElementById("age").value;
@@ -8,55 +16,65 @@ function generateQRCode() {
   var emergencyContact1 = document.getElementById("emergencyContact1").value;
 
   if (!name){
-    name = "Unkown"
+    name = "Unknown"
   }
 
 if (!age){
-  age = "age unknown"
+  age = "Unknown"
 }
 
   if (!allergies) {
-    allergies = "no allergies";
-  } else {
-    allergies = " has " + allergies.toLowerCase() + " allergy";
+    allergies = "None";
   }
 
 
   if (!bloodType) {
-    bloodType = "unknown bloodtype";
+    bloodType = "Unknown";
   } else {
-    bloodType = "Bloodtype: " + bloodType.toUpperCase();
+    bloodType.toUpperCase();
   }
 
 
   if (!medConditions) {
-    medConditions = "no underlying medical conditions";
+    medConditions = "None";
   }
 
 
   if(!emergencyContact1) {
     emergencyContact1 = "N/A"
   }
-  emergencyContact1 = "contact: " + emergencyContact1;
 
+
+  name=name.replace(/ /g,"_");
+  age=age.replace(/ /g,"_");
+  bloodType=bloodType.replace(/ /g,"_");
+  allergies=allergies.replace(/ /g,"_");
+  medConditions=medConditions.replace(/ /g,"_");
+  emergencyContact1=emergencyContact1.replace(/ /g,"_");
+  
 
 
 
   // Create user String with the input values
   var userInfo = name + ", " + age + ", " + bloodType + ", " + allergies + ", " + medConditions + ", " + emergencyContact1;
 
+  var patientInfoURL = "https://abduldavids.github.io/patientinfo.html?n=" + name + "&a=" + age + "&bl=" + bloodType + "&all=" + allergies + "&cd=" + medConditions + "&ct=" + emergencyContact1
+
+
+  console.log(patientInfoURL);
+
 
   // Generate the QR code using a QR code library
   var qrCode = new QRCode(document.getElementById("qrcode"), {
-    text: userInfo,
+    text: patientInfoURL,
     width: 300,
     height: 300,
   });
 
   // Create a download link for the QR code
   var downloadLink = document.createElement("a");
-  downloadLink.href = document.getElementById("qrcode").getElementsByTagName("canvas")[0].toDataURL("image/jpeg");
-  downloadLink.download = "qrcode.jpg";
-  downloadLink.innerHTML = "Download QR Code";
+  downloadLink.href = patientInfoURL;
+  downloadLink.innerHTML = "Generated Patient File";
   document.getElementById("qrcode-container").appendChild(downloadLink);
 }
+
